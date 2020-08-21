4768f436c8494fd34969385b3bce09d1895c1902
Sarai Berenice Castaño Saldívar <sbcastanos@jumex.com.mx>
2020-08-21 16:21:36 -0500

    proyecto2actualizacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (<label>Hola sarai</label> 
-  );
-}
-
-export default App;
+ export default class App extends React.Component {render(){return(
+<Navbar collapseOnSelect expand="lg" bg="primary" variant="dark"><Navbar.Brand>inventario</Navbar.Brand> 
+<Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
+<Navbar.Collapse id="responsive-navbar-nav"><Nav className="mr-auto">
+<Nav.Link>inicio</Nav.Link><Nav.Link>Buscar</Nav.Link><Nav.Link>Ayuda</Nav.Link>
+</Nav></Navbar.Collapse>
+</Navbar>
+ )}}
